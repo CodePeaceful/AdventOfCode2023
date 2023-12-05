ff0f635510181d51d0a16fbef281b61cd32527a5
Christian Schlecht <christian.schlecht@arcor.de>
2023-12-05 16:39:23 +0100

    Task 9 needs to safe memory map 0 to big for memory

--- Task9/AdventOfCode2023Task9/AdventOfCode2023Task9.cpp
+++ Task9/AdventOfCode2023Task9/AdventOfCode2023Task9.cpp
@@ -7,12 +7,12 @@
 #include <array>
 
 std::vector<std::string>* getInput(std::string filePath);
-int solve(std::vector<std::string>* lines);
+long long solve(std::vector<std::string>* lines);
 std::vector<std::string> split(std::string line, char seperator);
-void buildMaps(std::vector<std::string>* lines); 
+std::array<std::map<long long, long long>, 7> buildMaps(std::vector<std::string>* lines);
 std::vector<std::string> getMapData(std::vector<std::string>* lines, int mapId);
 
-std::array<std::map<int, int>, 7> maps;
+
 
 int main() {
     std::cout << solve(getInput(".\\input.txt"));
@@ -32,19 +32,19 @@ std::vector<std::string>* getInput(std::string filePath) {
     return file;
 }
 
-int solve(std::vector<std::string>* lines) {
+long long solve(std::vector<std::string>* lines) {
     int min = 2147483647;
     std::vector<std::string> seedStrings = split(split(lines->at(0), ':')[1], ' ');
-    std::vector<int> seeds;
+    std::vector<long long> seeds;
     for (std::string seedString : seedStrings) {
-        seeds.push_back(std::stoi(seedString));
+        seeds.push_back(std::stoll(seedString));
     }
-    buildMaps(lines);
+    std::array<std::map<long long, long long>, 7> maps = buildMaps(lines);
 
-    for (int seed : seeds) {
-        int value = seed;
-        for (std::map<int, int> map : maps) {
-            if (map.count(value)) {
+    for (long long seed : seeds) {
+        long long value = seed;
+        for (std::map<long long, long long> map : maps) {
+            if (map.contains(value)) {
                 value = map[value];
             }
         }
@@ -70,23 +70,25 @@ std::vector<std::string> split(std::string line, char seperator) {
     return splitString;
 }
 
-void buildMaps(std::vector<std::string>* lines) {
+std::array<std::map<long long, long long>, 7> buildMaps(std::vector<std::string>* lines) {
     int mapId = 0;
-    for (std::map<int,int> map : maps) {
+    std::array<std::map<long long, long long>, 7> maps;
+    for (int j = 0; j < maps.size(); j++) {
         std::vector<std::string> mapData = getMapData(lines, mapId);
 
         for (std::string line : mapData) {
-            int key, value, range;
+            long long key, value, range;
             std::vector<std::string> strings = split(line, ' ');
-            key = std::stoi(strings[0]);
-            value = std::stoi(strings[1]);
-            range = std::stoi(strings[2]);
-            for (int i = 0; i < range; i++) {
-                map.insert(std::pair<int,int>(key + i, value + i));
+            key = std::stoll(strings[1]);
+            value = std::stoll(strings[0]);
+            range = std::stoll(strings[2]);
+            for (long long i = 0; i < range; i++) {
+                maps[j].insert(std::pair<long long, long long>(key + i, value + i));
             }
         }
         mapId++;
     }
+    return maps;
 }
 
 std::vector<std::string> getMapData(std::vector<std::string>* lines, int mapId) {
